d7ec4ddcad4d41492701a00bcf5a5dde6e818dd5
Livens-coder <nicolaslivenson@gmail.com>
2023-11-03 15:15:38 -0400

    map avanse gooo

--- foncBase.py
+++ foncBase.py
@@ -6,7 +6,7 @@ print(chaine)
 print("-----------------------------------------------------")
 
 a=chaine.lower()
-print("mete tout karakte yo an miniski =>:" ,a)
+print("mete tout karakte yo an miniskil =>:" ,a)
 k=chaine.split(" ")
 print("koupe chenn nan tout kote ki gen espas =>:", k)
 b=chaine.title()
@@ -23,13 +23,13 @@ print("Mete yon chenn karakte devan deye, answit mete l an majiskil =>: ", nouvo
 print("endeks premye karakte <a> ki nan yon chenn =>:", chaine.index('a'))
 chenn=chaine.lower()
 endeks=0
-for el, karakte in enumerate(chenn):
-    if karakte == 'a':
+for el, karakte in enumerate(chaine):
+    if karakte == 'a' or karakte == 'A':
         endeks += el
 print("som endeks a nan chenn karakte a se =>: ", endeks)
 lis=[]
-for el, karakte in enumerate(chenn):
-    if karakte == 'a':
+for el, karakte in enumerate(chaine):
+    if karakte == 'a' or karakte == 'A':
         lis.append(el)
 print("yon lis ki gen endeks tout karakte 'a' ki nan yon chenn => :", lis)
 
@@ -37,5 +37,20 @@ k=chaine.replace(' ', '')
 t=len(k)
 print(" chenn karakte a san espas =>  {},  kantite karakte li chenn sa vin genyen => {}".format(k,t))
 
-## nimewo 2
-print("nap komanse nan pati 2 a la: ")
+print("nimewo 2")
+print("------------------------------------------------: ")
+k=[]
+
+for i in range(0,2):
+    a=int(input("rantre kek n: "))
+    if a%2 ==0:
+        k.append(a)
+        
+    else:
+        a=int(input("eseye anko nonb ou an pa divizib pa 2: "))
+        
+print("sa se lis eleman ou sezi a ki tou dizib pa 2", k)
+## pati 2
+a= ''.join(map(str,k))
+print("men lis la sou fom chenn karakte", a)
+
